b726e7345cf73df97b0e05b62ba57211aba9d9ca
M4ns0 <mrbrunomanso@gmail.com>
2020-03-26 10:27:37 -0300

    add

---
+++ Técnicas de Programação/Em Sala/Sorteio/src/sorteio/classeDado.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorteio;
+
+/**
+ *
+ * @author japa
+ */
+public class classeDado {
+    private int valorSelecionado;
+
+    public classeDado() {
+        this.valorSelecionado = 1;
+    }
+
+    public int getValorSelecionado() {
+        return valorSelecionado;
+    }
+
+    public void setValorSelecionado(int valorSelecionado) {
+        this.valorSelecionado = valorSelecionado;
+    }  
+}
